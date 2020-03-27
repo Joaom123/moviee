54958428c6f57b67faafe4a75247b70b406dc26e
João Marcus <joaomarcus12769@gmail.com>
2020-03-26 22:32:27 -0300

    base

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,11 +2,6 @@ import React, {Component} from "react";
 import "./styles.css"
 
 class Card extends Component {
-    /*
-    constructor(props) {
-        super(props);
-    }
-    */
 
     render() {
         return (
