3da0241023d3c872eca2420bbfa12b13423a0ac6
João Marcus <joaomarcus12769@gmail.com>
2020-03-30 23:09:56 -0300

    alterando nome do site

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './pages/Inicial/App';
+import Inicial from './pages/Inicial/Inicial';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Inicial />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/pages/Inicial/Inicial.js
@@ -0,0 +1,85 @@
+import React, {Component} from 'react';
+import SearchForm from "../../components/SearchForm";
+import Card from "../../components/Card";
+import "../../style.css";
+import "materialize-css/dist/css/materialize.min.css";
+import "materialize-css/dist/js/materialize.min";
+import apiService from "../../services/apiService";
+import Button from "../../components/Button";
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            searchValue: "",
+            page: 1,
+            filmes: [],
+        };
+
+        this.searchMovies = this.searchMovies.bind(this);
+        this.onButtonClick = this.onButtonClick.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    searchMovies (searchValue) {
+        apiService.ListaFilmes({searchValue})
+            .then(response => {
+                console.log(response);
+                if(response.data.Response === 'True'){
+                    this.setState({
+                        filmes: response.data.Search
+                    })
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    onButtonClick () {
+        let page = this.state.page + 1;
+        let searchValue = this.state.searchValue;
+
+        apiService.ListaFilmes({searchValue, page})
+        .then(response => {
+            if(response.data.Response === 'True'){
+                console.log(response);
+                this.setState({
+                    page: page,
+                    filmes: response.data.Search
+                });
+            }
+        })
+    }
+
+    handleChange (event) {
+        this.setState({
+            searchValue: event.target.value
+        });
+    }
+
+    render() {
+        return (
+            <main>
+                <div className="container">
+                    <SearchForm
+                        onSubmit={this.searchMovies}
+                        searchValue={this.state.searchValue}
+                        handleChange={this.handleChange}
+                    />
+                    <section className="grid-template">
+                        {
+                            this.state.filmes.map( filme =>
+                                <Card filme = {filme}/>
+                            )
+                        }
+                    </section>
+                    <Button onClick={this.onButtonClick}/>
+                </div>
+            </main>
+        );
+    }
+}
+
+export default App;
