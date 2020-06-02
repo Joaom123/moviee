a46bc108e199fe0106f34d5e1ca2c3899dfdc950
João Marcus <joaomarcus12769@gmail.com>
2020-06-02 00:44:33 -0300

    Abrir modal com informações do filme

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -5,46 +5,23 @@ import "materialize-css/dist/css/materialize.min.css";
 import "materialize-css/dist/js/materialize.min";
 
 class Modal extends Component {
-
     componentDidMount() {
         const options = {
-            onOpenStart: () => {
-                console.log("Open Start");
-            },
-            onOpenEnd: () => {
-                console.log("Open End");
-            },
-            onCloseStart: () => {
-                console.log("Close Start");
-            },
-            onCloseEnd: () => {
-                console.log("Close End");
-            },
-            inDuration: 250,
-            outDuration: 250,
-            opacity: 0.5,
+            inDuration: 300,
+            outDuration: 300,
+            opacity: 0.4,
             dismissible: true,
-            startingTop: "4%",
+            startingTop: "10%",
             endingTop: "10%"
         };
 
-
-
-        console.log(this.props.open)
-        if(this.props.open) {
-            M.Modal.init(this.Modal, options);
-            let instance = M.Modal.getInstance(this.Modal);
-            instance.open();
-
-        }
-        // instance.close();
-        // instance.destroy();
+        M.Modal.init(this.Modal, options);
     }
 
-    componentDidUpdate(prevProps) {
-        // Uso típico, (não esqueça de comparar as props):
-        if (this.props.open !== prevProps.open) {
-            console.log("oi")
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props.toggleModal === true) {
+            let instance = M.Modal.getInstance(this.Modal);
+            instance.open();
         }
     }
 
@@ -55,7 +32,7 @@ class Modal extends Component {
             <div
                 ref={ Modal => { this.Modal = Modal; } }
                 id="modal"
-                className="modal1"
+                className="modal"
             >
                 <div className="modal-content">
                     {movie.Title}
