682fb3f21009c1896fd1952f67c58485a42b8b93
João Marcus <joaomarcus12769@gmail.com>
2020-06-07 00:10:24 -0300

    começando informações do filme no modal

--- src/components/ModalMovie/index.jsx
+++ src/components/ModalMovie/index.jsx
@@ -4,17 +4,24 @@ import Modal from "../Modal";
 function ModalMovie({movie, toggleModal, onCloseModal}) {
     return (
         <Modal toggleModal={toggleModal} onCloseModal={onCloseModal}>
-            <div className="modalMovie">
-                <div className="modalMovie__poster">
-                    <img src={movie.Poster} alt="" />
+            <div className="modalMovie row">
+                <div className="modalMovie__poster col s12 m6">
+                    <img className="w-100" src={movie.Poster} alt="" />
                 </div>
-                <div className="modalMovie__info">
-
+                <div className="modalMovie__info col s12 m6">
+                    <div className="modalMovie__header">
+                        <h2 className="modalMovie__title">{movie.Title}</h2>
+                        <div>
+                            <span className="modalMovie__genre">{movie.Genre}</span>
+                        </div>
+                    </div>
+                    <div className="modalMovie__body">
+                        <p className="modalMovie__plot">{movie.Plot}</p>
+                    </div>
                 </div>
             </div>
         </Modal>
     );
-
 }
 
 export default ModalMovie;
