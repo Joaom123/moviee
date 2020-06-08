d8ea5d512a56ad9a1c2b40d4d249af19031a9db1
João Marcus <joaomarcus12769@gmail.com>
2020-06-07 23:45:34 -0300

    Adicionando novas informações sobre filme no modal | Alterando estilo

--- src/components/ModalMovie/index.jsx
+++ src/components/ModalMovie/index.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 import Modal from "../Modal";
+import "./modalMovie.css"
 
 function ModalMovie({movie, toggleModal, onCloseModal}) {
     return (
         <Modal toggleModal={toggleModal} onCloseModal={onCloseModal}>
-            <div className="modalMovie row">
-                <div className="modalMovie__poster col s12 m6">
-                    <img className="w-100" src={movie.Poster} alt="" />
+            <div className="row modalMovie">
+                <div className="col s12 m6 modalMovie__poster">
+                    <img className="w-100 h-100" src={movie.Poster} alt="" />
                 </div>
-                <div className="modalMovie__info col s12 m6">
+                <div className="col s12 m6 modalMovie__info">
                     <div className="modalMovie__header">
                         <h2 className="modalMovie__title">{movie.Title}</h2>
                         <div>
                             <span className="modalMovie__genre">{movie.Genre}</span>
+                            <div>
+                                <span className="modalMovie__rated">{movie.Rated}</span>
+                               <span className="modalMovie__runtime">{movie.Runtime}</span>
+                            </div>
                         </div>
                     </div>
                     <div className="modalMovie__body">
