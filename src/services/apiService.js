42ed45450815bf35cc72233da32cfc3b0ed99ddf
João Marcus <joaomarcus12769@gmail.com>
2020-06-09 20:33:30 -0300

    http -> https

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiKey = 'e7bf29e9';
-const url = 'http://www.omdbapi.com';
+const url = 'https://www.omdbapi.com';
 
 const apiService = {
     getMoviesList: ({searchValue, page = 1}) => axios.get(url, {
