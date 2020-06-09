ade018da07336ccde2c62c8c24e111817cee9169
João Marcus <joaomarcus12769@gmail.com>
2020-06-08 23:28:47 -0300

    css do modal com informações, falta apenas Rating | Score | Awards

--- src/components/ModalMovie/index.jsx
+++ src/components/ModalMovie/index.jsx
@@ -6,13 +6,13 @@ function ModalMovie({movie, toggleModal, onCloseModal}) {
     return (
         <Modal toggleModal={toggleModal} onCloseModal={onCloseModal}>
             <div className="row modalMovie">
-                <div className="col s12 m6 modalMovie__poster">
+                <div className="col s12 m5 modalMovie__poster">
                     <img className="w-100 h-100" src={movie.Poster} alt="" />
                 </div>
-                <div className="col s12 m6 modalMovie__info">
+                <div className="col s12 m7 modalMovie__info">
                     <div className="modalMovie__header">
                         <h2 className="modalMovie__title">{movie.Title}</h2>
-                        <div className="row">
+                        <div className="row" style={{marginBottom: "0"}}>
                             <span className="col s12 l6 modalMovie__genre">{movie.Genre}</span>
                             <div className="col s12 l6 modalMovie__ratedAndRuntime">
                                 <span className="modalMovie__rated">{movie.Rated}</span>
