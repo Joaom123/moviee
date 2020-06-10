4690a8491cb6248751f1701251a21e5487cf2589
João Marcus <joaomarcus12769@gmail.com>
2020-06-09 21:35:32 -0300

    alterando nome do aplicativo

--- src/components/index.js
+++ src/components/index.js
@@ -4,4 +4,4 @@ import SearchForm from "./SearchForm";
 import Pagination from "./Pagination";
 import ModalMovie from "./ModalMovie";
 
-export { Modal, Movies, SearchForm, Pagination, ModalMovie };
+export {Modal, Movies, SearchForm, Pagination, ModalMovie};
