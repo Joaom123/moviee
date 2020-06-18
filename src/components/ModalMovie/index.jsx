5cd533439c5e77267020301d2de376c3ce4487ac
Joaom123 <joaomarcus12769@gmail.com>
2020-06-17 23:35:29 -0300

    começando rating e awards

--- src/components/ModalMovie/index.jsx
+++ src/components/ModalMovie/index.jsx
@@ -22,7 +22,7 @@ function MoviePoster({movie}) {
             <img
                 className="w-100 h-100"
                 src={getMoviePosterOrDefaultPoster(movie)}
-                alt=""
+                alt="Poster do filme"
             />
         </div>
     );
@@ -38,36 +38,69 @@ function MovieInfo({movie}) {
 }
 
 function MovieHeader({movie}) {
+    const {Title, Genre} = movie;
+
     return (
         <div className="modalMovie__header">
-            <h2 className="modalMovie__title">{movie.Title}</h2>
-            <div className="row modalMovie__" style={{marginBottom: "0"}}>
-                <span className="col s12 l6 modalMovie__genre">{movie.Genre}</span>
+            <h2 className="modalMovie__title">{Title}</h2>
+            <div className="row modalMovie__genreAndBadges">
+                <span className="col s12 l6 modalMovie__genre">{Genre}</span>
                 <MovieBadges movie={movie} />
             </div>
             <hr />
-            <div>
-
+            <div className="row modalMovie__ratingAndAwards">
+                <MovieRating movie={movie} />
+                <MovieAwards movie={movie} />
             </div>
         </div>
     );
 }
 
 function MovieBadges({movie}) {
+    const {Year, Rated, Runtime} = movie;
+
     return (
         <div className="col s12 l6 modalMovie__badges">
-            <span className="modalMovie__badgePill">{movie.Year}</span>
-            <span className="modalMovie__badgePill">{movie.Rated}</span>
-            <span className="modalMovie__badgePill">{movie.Runtime}</span>
+            <span className="modalMovie__badgePill">{Year}</span>
+            <span className="modalMovie__badgePill">{Rated}</span>
+            <span className="modalMovie__badgePill">{Runtime}</span>
         </div>
     );
 }
 
+function MovieRating({movie}) {
+    const {imdbRating, imdbVotes} = movie;
+
+    const ratingPercentagem = parseFloat(imdbRating)*10;
+
+    return (
+        <div className="modalMovie__rating">
+            <span>{imdbRating}</span>
+            <span className="modalMovie__votes">{imdbVotes}</span>
+        </div>
+    );
+}
+
+function MovieAwards({movie}) {
+    const {Awards} = movie;
+
+    const hasAward = Awards !== "N/A";
+
+    //TODO: Se tiver, exibe trofeu coloriodo com popover, se não,
+
+    return (
+        <div className="modalMovie__rating">
+            <span>{Awards}</span>
+        </div>
+    );
+}
 
 function MovieBody({movie}) {
+    const {Plot} = movie;
+
     return (
         <div className="modalMovie__body">
-            <p className="modalMovie__plot">{movie.Plot}</p>
+            <p className="modalMovie__plot">{Plot}</p>
         </div>
     );
 }
