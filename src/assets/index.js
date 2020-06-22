2853064f26722844c17be85c41ecdc8258ab2e06
Joaom123 <joaomarcus12769@gmail.com>
2020-06-21 21:30:15 -0300

    rating.png -> rating.svg

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,5 @@
 import trophyIcon from "./img/trophy.png";
-import ratingIcon from "./img/rating.png";
+import ratingIcon from "./img/rating.svg";
 import noPoster from "./img/no-poster.jpg";
 
 export {trophyIcon, ratingIcon, noPoster};
