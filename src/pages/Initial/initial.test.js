7821c2069fddaf801e5918c262bca7365e0efab8
Joaom123 <joaomarcus12769@gmail.com>
2020-06-25 23:57:10 -0300

    testes no search input

--- src/pages/Initial/initial.test.js
+++ src/pages/Initial/initial.test.js
@@ -1,10 +1,36 @@
 import React from "react";
-import { render, cleanup } from "@testing-library/react";
+import {render, cleanup, fireEvent} from "@testing-library/react";
 import Initial from "./index";
 
 afterEach(cleanup);
 
-it('snapshot ', () => {
-    const {asFragment} = render(<Initial />);
-    expect(asFragment(<Initial />)).toMatchSnapshot();
+const setUp = {
+    searchInputNode : () => {
+        const {getByTestId} = render(<Initial />);
+        return getByTestId("searchMovieInput");
+    }
+}
+
+describe("initial page", () => {
+    it('snapshot', () => {
+        const {asFragment} = render(<Initial />);
+        expect(asFragment()).toMatchSnapshot();
+    });
+
+    describe("input search", () => {
+        const testSearchInputValueChange = (inputValue, expectedValue) => {
+            const searchInputNode = setUp.searchInputNode();
+
+            fireEvent.change(searchInputNode, {target: {value: inputValue}});
+            expect(searchInputNode.value).toBe(expectedValue);
+        }
+
+        it('should not accept space at the beginning', function () {
+            testSearchInputValueChange(" ", "");
+        });
+
+        it('should accept changing value', function () {
+            testSearchInputValueChange("test test", "test test");
+        });
+    })
 });
